4c2fbeb502caaa8b5fdb9a4d55c270f68cc5875d
Truong Nguyen <truong.nguyen@team.telstra.com>
2020-03-25 15:33:29 +1100

    update cron

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 # sudo chmod -R 777 data
 
+# #iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 4000
+
+#service iptables save
+#service iptables restart
+
 docker-compose down && docker-compose up --remove-orphans -d --build && docker-compose logs -f grafana job_runner influxdb chronograf
